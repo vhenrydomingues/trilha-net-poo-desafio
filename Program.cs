0758ec565b5dbcf48bc25ff9588e8e8bdd80cc3f
HENRY <vhenrydomingues@gmail.com>
2025-01-09 10:05:56 -0300

    projeto de estudo de POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "970707070", modelo: "Modelo 1", imei: "111111111", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Genshin Impact");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "960706070", modelo: "Modelo 2", imei: "222222222", memoria: 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Ragnarok Origin");
